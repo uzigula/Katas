62aaef808d6592d7074d1ea73d36a5232cdb8186
Uzi Mamani <uzigula@gmail.com>
2013-04-15 15:43:01 -0500

    Evaluamos Registro de Puntuacion

--- Tennis_CSharp/Tennis.Test/TennisScoreTest.cs
+++ Tennis_CSharp/Tennis.Test/TennisScoreTest.cs
@@ -1,17 +1,76 @@
 ﻿using NUnit.Framework;
+using Tennis;
+
 namespace TennisScore.Test
 {
     [TestFixture]
     public class TestScoreTest
     {
+        private TennisGame  tennis ;
+
+        [SetUp]
+        public void Setup()
+        {
+            tennis = new Tennis.TennisGame("Federer", "Nadal");
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            tennis = null;
+        }
+
         [Test]
         public void CuandoSeIniciaElJuego_EntoncesElMarcado_DebeSerEmpateACero()
         {
-            var tennis = new Tennis.TennisGame("Federer", "Nadal");
             var score = tennis.Score();
             Assert.AreEqual("Federer: 0 Nadal: 0", score);
         }
 
-      
-}
+        [Test]
+        public void DadoJuegoIniciado_Jugador1GanaUnPunto_EntoncesScore_15_A_0()
+        {
+            tennis.PuntoPara("Federer");
+            var score = tennis.Score();
+            Assert.AreEqual("Federer: 15 Nadal: 0", score);
+        }
+
+        [Test]
+        public void DadoJuegoIniciado_Jugador2GanaUnPunto_EntoncesScore_0_A_15()
+        {
+            tennis.PuntoPara("Nadal");
+            var score = tennis.Score();
+            Assert.AreEqual("Federer: 0 Nadal: 15", score);
+        }
+
+        [Test]
+        public void DadoJuegoIniciado_Jugador1GanaDosPuntos_EntoncesScore_30_A_0()
+        {
+            tennis.PuntoPara("Federer");
+            tennis.PuntoPara("Federer");
+            var score = tennis.Score();
+            Assert.AreEqual("Federer: 30 Nadal: 0", score);
+        }
+        [Test]
+        public void DadoJuegoIniciado_Jugador2GanaDosPuntos_EntoncesScore_0_A_30()
+        {
+            tennis.PuntoPara("Nadal");
+            tennis.PuntoPara("Nadal");
+            var score = tennis.Score();
+            Assert.AreEqual("Federer: 0 Nadal: 30", score);
+        }
+
+        [Test]
+        public void DadoJuegoIniciado_Jugador1GanaTresPuntosYJugador2GanaDosPuntos_EntoncesScore_40_A_30()
+        {
+            tennis.PuntoPara("Nadal");
+            tennis.PuntoPara("Federer");
+            tennis.PuntoPara("Federer");
+            tennis.PuntoPara("Nadal");
+            tennis.PuntoPara("Federer");
+            var score = tennis.Score();
+            Assert.AreEqual("Federer: 40 Nadal: 30", score);
+        }
+
+    }
 }
--- Tennis_CSharp/Tennis/TennisGame.cs
+++ Tennis_CSharp/Tennis/TennisGame.cs
@@ -9,8 +9,8 @@ namespace Tennis
 
         public TennisGame(string jugador1, string jugador2)
         {
-           _jugador1.Nombre = jugador1;
-           _jugador2.Nombre = jugador2;
+            _jugador1.Nombre = jugador1;
+            _jugador2.Nombre = jugador2;
         }
 
         public string Score()
@@ -18,23 +18,45 @@ namespace Tennis
             return string.Format(_jugador1.Nombre + ": {0} " + _jugador2.Nombre + ": {1}", _jugador1.Score, _jugador2.Score);
         }
 
-  
+        public void PuntoPara(string nombreJugador)
+        {
+            var jugador = ObtieneJugador(nombreJugador);
+            jugador.Anotacion();
+        }
+
+        private Jugador ObtieneJugador(string nombreJugador)
+        {
+            if (_jugador1.Nombre == nombreJugador)
+                return _jugador1;
+            else if (_jugador2.Nombre == nombreJugador)
+                return _jugador2;
+            else
+                return null;
+        }
+
     }
 
     internal class Jugador
     {
-        private int _score=0;
+        private int _score = 0;
         public string Nombre { get; set; }
 
         public int Score
         {
-            get { return _score; }
+            get
+            {
+                return _score;
+            }
         }
 
         public void Anotacion()
         {
-            
+            _score += 15;
+            if (_score > 40)
+                _score = 40;
+
         }
+
 
     }
 }
